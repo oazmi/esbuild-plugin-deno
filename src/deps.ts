e44ea2cbb8923b0c3ebfeb5577eb4a2898ce0398
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2025-01-19 16:14:07 -0500

    - add submodule `/src/loadermap/mod.ts`, which exports the function `guessHttpResponseLoaders`, that predicts a narrow set of esbuild loaders appropriate for loading the resources of a given http response.   it takes into account the mime type (present inside the response's header) and the url path's extension, to propose the appropriate esbuild loader types. - add more dependencies to `/src/deps.ts`.

--- src/deps.ts
+++ src/deps.ts
@@ -1,3 +1,21 @@
-export { } from "@oazmi/kitchensink"
+import { getUriScheme, resolvePathFactory } from "@oazmi/kitchensink/pathman"
+
+
+export { json_stringify, object_entries, response_json } from "@oazmi/kitchensink/alias"
+export { InvertibleMap, invertMap } from "@oazmi/kitchensink/collections"
+export { ensureEndSlash, ensureStartDotSlash, joinPaths, parseFilepathInfo, parsePackageUrl, resolveAsUrl } from "@oazmi/kitchensink/pathman"
 export type * as esbuild from "npm:esbuild@0.24.2"
 
+// below is a custom path segment's absoluteness test function that will identify all `UriScheme` segments that are not "relative" as absolute paths,
+export const isAbsolutePath = (segment: string) => {
+	const scheme = getUriScheme(segment) ?? "relative"
+	return scheme !== "relative"
+}
+
+export const
+	getCwd = () => Deno.cwd(),
+	defaultResolvePath = resolvePathFactory(getCwd, isAbsolutePath)
+
+export const
+	textEncoder = new TextEncoder(),
+	textDecoder = new TextDecoder()
