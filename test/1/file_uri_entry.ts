8a6563c08288644b7131c7176fef97868e276757
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2025-01-26 00:12:01 -0500

    - update the test `/test/1/` to showcase import-map support, along with duplicate code resource re-usage (i.e. despite the different aliases, the absolute resolved path is the same, therefore the resource is declared as one and the same).

--- test/1/file_uri_entry.ts
+++ test/1/file_uri_entry.ts
@@ -1,3 +1,4 @@
+import { meshGrid } from "2d-array-utils" // this is referenced in our plugin's `globalImportMap` setting
 import { transpose2D } from "https://jsr.io/@oazmi/kitchensink/0.9.2/src/numericarray.ts"
 import "https://raw.githubusercontent.com/jenil/chota/7d780731421fc987d8f7a1c8f66c730d8573684c/src/chota.css"
 import * as http from "node:http"
@@ -5,6 +6,7 @@ import * as https from "node:https"
 
 console.log("yahaha!", transpose2D([[1], [2], [3]]))
 console.log(http.Agent)
+console.log(meshGrid([1, 2, 3], [0, - 1, -2, -3, -4]))
 
 export { http, https }
 
--- test/1/mod.test.ts
+++ test/1/mod.test.ts
@@ -9,11 +9,12 @@ Deno.test("test http plugin", async () => {
 		"./local_path_entry.ts",
 		"file://" + import.meta.dirname + "/file_uri_entry.ts",
 		"https://jsr.io/@oazmi/kitchensink/0.9.2/src/array1d.ts",
-		"https://jsr.io/@oazmi/kitchensink/0.9.2/src/array2d.ts",
+		"https://2d-lib", // will bundle via our `globalImportMap` setting
+		// "2d-array-utils", // this import-map path will not work, because it will not pass though our plugin's /^https?:/ filter.
+		// and thereby, our `globalImportMap` will not be applied to it. instead, esbuild will try to resolve it as a local file.
 		"https://jsr.io/@oazmi/kitchensink/0.9.2/src/struct.ts",
 		"https://raw.githubusercontent.com/jenil/chota/7d780731421fc987d8f7a1c8f66c730d8573684c/src/chota.css",
-	]
-	.map((path) => ({
+	].map((path) => ({
 		in: path,
 		out: parseFilepathInfo(path).basename
 	}))
@@ -30,7 +31,15 @@ Deno.test("test http plugin", async () => {
 		treeShaking: true,
 		write: false,
 		metafile: true,
-		plugins: [{ name: "my-http-plugin", setup: httpPluginSetup() }],
+		plugins: [{
+			name: "my-http-plugin",
+			setup: httpPluginSetup({
+				globalImportMap: {
+					"2d-array-utils": "https://jsr.io/@oazmi/kitchensink/0.9.2/src/array2d.ts",
+					"https://2d-lib": "https://jsr.io/@oazmi/kitchensink/0.9.2/src/array2d.ts",
+				}
+			})
+		}],
 	})
 
 	console.log(result.outputFiles.map((v) => (v.path)))
