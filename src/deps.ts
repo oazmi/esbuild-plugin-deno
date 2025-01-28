60bc1cb094e4664782a9e069ab7c946e78d78141
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2025-01-28 07:03:58 -0500

    - in `/src/importmap/typedefs.ts`, add the abstract class `RuntimePackageMetadata`, which provides a generic interface for querying a generic js-runtime package's import and export aliases (i.e. import-maps and export-maps). - in `/src/deps.ts`, add new dependencies for parsing package metadata (which will be used in future commits):   - `jsr:@std/jsonc` is used for jsonc and json file parsing.   - `jsr:@std/semver` is used for selecting an appropriate version of a package, given a version range criteria.

--- src/deps.ts
+++ src/deps.ts
@@ -1,10 +1,13 @@
 import { getUriScheme, resolvePathFactory } from "@oazmi/kitchensink/pathman"
 
 
-export { json_parse, json_stringify, object_entries, object_keys, object_values } from "@oazmi/kitchensink/alias"
+export { json_parse, json_stringify, object_assign, object_entries, object_fromEntries, object_keys, object_values, promise_resolve } from "@oazmi/kitchensink/alias"
 export { InvertibleMap, invertMap } from "@oazmi/kitchensink/collections"
 export { ensureEndSlash, ensureStartDotSlash, joinPaths, normalizePath, parseFilepathInfo, parsePackageUrl, resolveAsUrl } from "@oazmi/kitchensink/pathman"
-export type { Optional } from "@oazmi/kitchensink/typedefs"
+export { isString } from "@oazmi/kitchensink/struct"
+export type { ConstructorOf, Optional } from "@oazmi/kitchensink/typedefs"
+export { parse as jsoncParse } from "@std/jsonc"
+export { maxSatisfying as semverMaxSatisfying, minSatisfying as semverMinSatisfying, parse as semverParse, parseRange as semverParseRange, format as semverToString } from "@std/semver"
 export type * as esbuild from "npm:esbuild@0.24.2"
 
 /** flags used for minifying (or eliminating) debugging logs and asserts, when an intelligent bundler, such as `esbuild`, is used. */
