482dae31c6eb27cd39359e122db5d2f503306417
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2025-01-30 07:39:22 -0500

    - update the test `/test/1/` to showcase our portable jsr package resolution support via the new `jsrPluginSetup` plugin.

--- test/1/local_path_entry.ts
+++ test/1/local_path_entry.ts
@@ -2,13 +2,14 @@
 // since we will be in the local "file" namespace when this file is loaded by esbuild.
 import { meshGrid } from "2d-array-utils"
 import { transpose2D } from "https://jsr.io/@oazmi/kitchensink/0.9.2/src/numericarray.ts"
+import { cumulativeSum } from "jsr:@oazmi/kitchensink@~0.9.2/numericarray"
 import "https://raw.githubusercontent.com/jenil/chota/7d780731421fc987d8f7a1c8f66c730d8573684c/src/chota.css"
 import * as http from "node:http"
 import * as https from "node:https"
 
 console.log("yahaha!", transpose2D([[1], [2], [3]]))
 console.log(http.Agent)
-console.log(meshGrid([1, 2, 3], [0, - 1, -2, -3, -4]))
+console.log(meshGrid([1, 2, 3], [0, - 1, -2, -3, -4]), cumulativeSum([1, 2, 3, 4]))
 
 export { http, https }
 
--- test/1/mod.test.ts
+++ test/1/mod.test.ts
@@ -3,17 +3,20 @@ import { parseFilepathInfo } from "@oazmi/kitchensink/pathman"
 import esbuild from "npm:esbuild@0.24.2"
 import { httpPluginSetup } from "../../src/plugins/http.ts"
 import { importMapPluginSetup } from "../../src/plugins/importmap.ts"
+import { jsrPluginSetup } from "../../src/plugins/jsr.ts"
 
 
 Deno.test("test http plugin", async () => {
 	const entry_points = [
-		"./local_path_entry.ts",
-		"file://" + import.meta.dirname + "/file_uri_entry.ts",
-		"https://jsr.io/@oazmi/kitchensink/0.9.2/src/array1d.ts",
+		"./local_path_entry.ts", // this will be resolved and loaded by esbuild natively (although it will initially pass though the import-map-plugin resolver)
+		"file://" + import.meta.dirname + "/file_uri_entry.ts", // this will be resolved and loaded by our http-plugin
+		// "https://jsr.io/@oazmi/kitchensink/0.9.2/src/array1d.ts",
+		"jsr:@oazmi/kitchensink@^0.9.2/array1d", // this will be resolved by our jsr-plugin, and loaded by the http-plugin
 		"https://2d-lib", // this will bundle via our `globalImportMap` setting inside of the http-plugin
 		"2d-array-utils", // this will bundle via our `importMap` setting inside of our import-map-plugin
-		"https://jsr.io/@oazmi/kitchensink/0.9.2/src/struct.ts",
-		"https://raw.githubusercontent.com/jenil/chota/7d780731421fc987d8f7a1c8f66c730d8573684c/src/chota.css",
+		// "https://jsr.io/@oazmi/kitchensink/0.9.3/src/struct.ts",
+		"jsr:@oazmi/kitchensink/struct",         // this will be resolved to the latest version of the package by our jsr-plugin
+		"https://raw.githubusercontent.com/jenil/chota/7d780731421fc987d8f7a1c8f66c730d8573684c/src/chota.css", // http-plugin resolution and loading
 	].map((path) => ({
 		in: path,
 		out: "entry-" + parseFilepathInfo(path).basename
@@ -36,7 +39,7 @@ Deno.test("test http plugin", async () => {
 				name: "my-importmap-plugin",
 				setup: importMapPluginSetup({
 					importMap: {
-						"2d-array-utils": "https://jsr.io/@oazmi/kitchensink/0.9.2/src/array2d.ts",
+						"2d-array-utils": "jsr:@oazmi/kitchensink@0.9.2/array2d",
 					}
 				})
 			},
@@ -48,6 +51,10 @@ Deno.test("test http plugin", async () => {
 						"https://2d-lib": "https://jsr.io/@oazmi/kitchensink/0.9.2/src/array2d.ts",
 					}
 				})
+			},
+			{
+				name: "my-jsr-plugin",
+				setup: jsrPluginSetup()
 			}
 		],
 	})
