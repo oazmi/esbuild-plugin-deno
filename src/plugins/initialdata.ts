53ae6ac51f0d097926756f23add46015f963cd47
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2025-02-21 22:16:00 -0500

    - in `/src/plugins/initialdata.ts`, add a new configuration option `captureDependencies`:   - this makes the initial-data plugin keep a record of the entry-points and entities that it injects its `pluginData` into, so that these entities' dependencies can also be injected with the same `pluginData`.     this is needed because esbuild's native path-resolvers and path-loaders do not preserve the `pluginData`.     hence, without this feature, the initial `pluginData` will not propagate to the entry-points' dependencies, and so forth.

--- src/plugins/initialdata.ts
+++ src/plugins/initialdata.ts
@@ -7,7 +7,7 @@
  * @module
 */
 
-import type { esbuild } from "../deps.ts"
+import { normalizePath, type esbuild } from "../deps.ts"
 
 
 /** a symbol that gets implanted into resolving-entry's `pluginData`,
@@ -40,6 +40,21 @@ export interface ResolveInitialDataFactoryConfig<T = any> {
 	*/
 	pluginDataMarker: symbol
 
+	/** specify if you wish for the dependencies of the captured `"entry-points"` to be captured as well?
+	 * 
+	 * _why_? you ask? that's because esbuild's native loaders do not propagate the `pluginData` passed by the resolver, onto the dependency entities.
+	 * this means that the initial-data will be lost when a dependency of an entry-point is being resolved, due to esbuild's native loader not preserving it.
+	 * thus, our plugin will have to keep track of which entities it has already pushed its initial-data onto,
+	 * and then be on the lookout for dependency entities that come from an importer file that has had the initial-data injected into it,
+	 * so that this dependency entity can also be injected with the same initial-data.
+	 * 
+	 * if you don't want the initial-data to be injected onto the entry-point's dependencies (and deep grandchildren dependencies),
+	 * make sure to turn off this option, otherwise it'll be enabled by default.
+	 * 
+	 * @defaultValue `true`
+	*/
+	captureDependencies: boolean
+
 	/** aside from the `pluginData`, you can provide additional path-resolution-args to override when `build.resolve`
 	 * is called inside the body of {@link onResolveInitialDataFactory}, so that you can mutate properties of the entrypoint.
 	 * 
@@ -47,6 +62,8 @@ export interface ResolveInitialDataFactoryConfig<T = any> {
 	 * - setting `onResolveArgs.namespace = "oazmi-http"` will mean that you've effectively redirected the path resolution to the `"oazmi-http"` namespace.
 	 *   this can be a tactic to redirect all desired inputs to a specific namespace for resolution.
 	 * - setting `onResolveArgs.resolveDir` can let you specify the `"node_modules"`'s parent directory in which esbuild should scan for npm imports.
+	 * 
+	 * @defaultValue `{}` (no overwrites applied)
 	*/
 	onResolveArgs?: Partial<Omit<esbuild.OnResolveArgs, "pluginData" | "path">>
 }
@@ -69,6 +86,7 @@ export interface InitialDataPluginSetupConfig<T = any> extends ResolveInitialDat
 const defaultResolveInitialDataFactoryConfig: ResolveInitialDataFactoryConfig<any> = {
 	pluginData: {},
 	pluginDataMarker: CAPTURED_ALREADY,
+	captureDependencies: true,
 	onResolveArgs: {},
 }
 
@@ -79,20 +97,33 @@ const defaultInitialDataPluginSetupConfig: InitialDataPluginSetupConfig<any> = {
 }
 
 const onResolveInitialDataFactory = <T>(config: Partial<ResolveInitialDataFactoryConfig<T>>, build: esbuild.PluginBuild) => {
-	const { pluginData: initialPluginData, pluginDataMarker, onResolveArgs } = { ...defaultResolveInitialDataFactoryConfig, ...config }
+	const
+		{ pluginData: initialPluginData, pluginDataMarker, captureDependencies, onResolveArgs } = { ...defaultResolveInitialDataFactoryConfig, ...config },
+		captured_resolved_paths = new Set<string>()
 
 	return async (args: esbuild.OnResolveArgs): Promise<esbuild.OnResolveResult | undefined> => {
 		const
 			{ path, pluginData = {}, ...rest_args } = args,
-			merged_plugin_data = { ...initialPluginData, ...pluginData } as InitialPluginData<typeof pluginDataMarker>
-		// if the `kind` is not an entrypoint, then skip, since this plugin is intended to insert plugin data only to entry points.
-		if (rest_args.kind !== "entry-point") { return undefined }
+			merged_plugin_data = { ...initialPluginData, ...pluginData } as InitialPluginData<typeof pluginDataMarker>,
+			{ kind, importer } = rest_args
+
+		// if the `kind` is not an entrypoint, nor is the `importer` one of the files that has been previously captured for initial-data injection,
+		// then skip, since this plugin is intended to insert plugin data only to entry points, and their dependencies.
+		if (kind !== "entry-point" && !captured_resolved_paths.has(normalizePath(importer))) { return undefined }
+
 		// if the plugin marker already exists for this entity, then we've already processed it once,
 		// therefore we should return `undefined` so that we don't end in an infinite onResolve recursion.
 		// this way, the next resolver registered to esbuild (or its native resolver) will take up the task for resolving this entity.
 		if (merged_plugin_data[pluginDataMarker]) { return undefined }
 		merged_plugin_data[pluginDataMarker] = true
-		return build.resolve(path, { ...rest_args, ...onResolveArgs, pluginData: merged_plugin_data })
+
+		const resolved_result = await build.resolve(path, { ...rest_args, ...onResolveArgs, pluginData: merged_plugin_data })
+		// once again, if esbuild's native resolver resolved the `path`, then the `merged_plugin_data` that we just inserted will be lost.
+		// i.e. `resolved_result.pluginData === undefined` if esbuild's native resolver took care of the path-resolution.
+		// in such cases, we would like to re-insert our `merged_plugin_data` again, before returning the result
+		if (resolved_result.pluginData === undefined) { resolved_result.pluginData = merged_plugin_data }
+		if (captureDependencies) { captured_resolved_paths.add(normalizePath(resolved_result.path)) }
+		return { ...resolved_result, pluginData: merged_plugin_data }
 	}
 }
 
