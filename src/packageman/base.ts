8a7920ec4f9ce35cdbf537980e553dd0ce2d3c9c
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2025-04-21 08:33:47 +0300

    - create the branch `feature/workspaces`, where we'll add support for deno workspaces. - in `/src/packageman/base.ts`:   - make the `RuntimePackage.fromUrl` static method memorize the instance that is created from the given url.   - add the interfaces related to workspaces in the `RuntimePackage` base class.

--- src/packageman/base.ts
+++ src/packageman/base.ts
@@ -9,6 +9,23 @@ import { resolvePathFromImportMapEntries, type ResolvePathFromImportMapEntriesCo
 import type { ImportMapSortedEntries } from "../importmap/typedefs.ts"
 
 
+export interface RuntimePackageResolveImportConfig extends ResolvePathFromImportMapEntriesConfig {
+	/** a list of workspaces that should also be traversed in case the import resolution fails. */
+	workspaces: Array<RuntimePackage<any>>
+
+	/** once a workspace runtime-package has been visited, its path (returned by {@link RuntimePackage.getPath}) is saved to this `Set`,
+	 * so that it is not traversed again by any other deep child workspace during the current path-resolution task.
+	*/
+	visitedWorkspaces?: Set<string>
+}
+
+const defaultRuntimePackageResolveImportConfig: Omit<RuntimePackageResolveImportConfig, keyof ResolvePathFromImportMapEntriesConfig> = {
+	workspaces: [],
+	visitedWorkspaces: undefined,
+}
+
+const cachedRuntimePackage = new Map<string, RuntimePackage<any>>()
+
 /** an abstraction for import-map utilities of a general javascript runtime's package object with the schema `SCHEMA`.
  * - in the case of node, `SCHEMA` would represent `package.json`'s schema.
  * - in the case of deno, `SCHEMA` would represent `deno.json`, `deno.jsonc`, or `jsr.json`'s schema.
@@ -16,6 +33,21 @@ import type { ImportMapSortedEntries } from "../importmap/typedefs.ts"
  * @template SCHEMA a record type representing the package schema.
 */
 export abstract class RuntimePackage<SCHEMA extends Record<string, any>> {
+	/** specify all child workspaces of this package.
+	 * 
+	 * the child-workspaces will also inherit the import/export-resolution of this (parent) package,
+	 * and thereby, they will also implicitly inherit the import/export-resolution of their sibling packages,
+	 * nephew packages, children of nephew packages, and so on.
+	 * 
+	 * > [!important]
+	 * > the constructor of the subclasses do **not** typically parse the workspace paths from the provided schema,
+	 * > nor do they load the {@link RuntimePackage} associated with those workspaces,
+	 * > since it would require asynchronous operations (such as `fetch`) which cannot be performed inside the constructor.
+	 * > this is why you would either need to manually add/push your child-workspace {@link RuntimePackage} object,
+	 * > or use the asynchronous {@link fromUrl} static method in the subclasses to take care of auto-loading the children workspaces.
+	*/
+	public readonly childWorkspaces: Array<RuntimePackage<any>>
+
 	/** the path or url of the package json(c) file.
 	 * 
 	 * the {@link RuntimePackage | base class} does nothing with this information;
@@ -47,6 +79,7 @@ export abstract class RuntimePackage<SCHEMA extends Record<string, any>> {
 	constructor(package_object: SCHEMA, package_path: string) {
 		this.packageInfo = package_object
 		this.packagePath = package_path
+		this.childWorkspaces = []
 	}
 
 	/** get the package's name. */
@@ -69,7 +102,7 @@ export abstract class RuntimePackage<SCHEMA extends Record<string, any>> {
 	 * > [!tip]
 	 * > for test case examples and configuration options, see the documentation comments of {@link resolvePathFromImportMapEntries}
 	*/
-	resolveExport(path_alias: string, config?: Partial<ResolvePathFromImportMapEntriesConfig>): string | undefined {
+	resolveExport(path_alias: string, config?: Partial<RuntimePackageResolveImportConfig>): string | undefined {
 		return resolvePathFromImportMapEntries(path_alias, this.exportMapSortedEntries, { sort: false, ...config })
 	}
 
@@ -81,11 +114,20 @@ export abstract class RuntimePackage<SCHEMA extends Record<string, any>> {
 	 * > [!tip]
 	 * > for test case examples and configuration options, see the documentation comments of {@link resolvePathFromImportMapEntries}
 	*/
-	resolveImport(path_alias: string, config?: Partial<ResolvePathFromImportMapEntriesConfig>): string | undefined {
+	resolveImport(path_alias: string, config?: Partial<RuntimePackageResolveImportConfig>): string | undefined {
 		return resolvePathFromImportMapEntries(path_alias, this.importMapSortedEntries, { sort: false, ...config })
 	}
 
 	/** create an instance of this class by loading a package's json(c) file from a url or local file-system path.
+	 * 
+	 * > [!important]
+	 * > the resulting new instance is cached (memorized), so that it can be reused if another query with the same normalized path is provided.
+	 * > 
+	 * > _why are we forcing a cache mechanism on the base class?_
+	 * > 
+	 * > because {@link childWorkspaces} are referenced by their absolute path,
+	 * > and resolving an import through a child-workspace would involve the creation of that child workspace package via this method,
+	 * > thus leading to an exponential number of redundant re-creation of identical package manager objects.
 	 * 
 	 * > [!tip]
 	 * > the constructor uses a "JSONC" parser (from [@oazmi/kitchensink/stringman](https://jsr.io/@oazmi/kitchensink/0.9.10/src/stringman.ts)) for the fetched files.
@@ -96,7 +138,14 @@ export abstract class RuntimePackage<SCHEMA extends Record<string, any>> {
 		INSTANCE = RuntimePackage<SCHEMA>,
 	>(this: ConstructorOf<INSTANCE, [SCHEMA, string]>, package_jsonc_path: URL | string): Promise<INSTANCE> {
 		package_jsonc_path = resolveAsUrl(package_jsonc_path, defaultResolvePath())
-		const package_object = json_parse(jsoncRemoveComments(await ((await fetch(package_jsonc_path, defaultFetchConfig)).text()))) as SCHEMA
-		return new this(package_object, package_jsonc_path.href)
+		const
+			package_jsonc_path_str = package_jsonc_path.href,
+			cached_result = cachedRuntimePackage.get(package_jsonc_path_str)
+		if (cached_result) { return cached_result as INSTANCE }
+		const
+			package_object = json_parse(jsoncRemoveComments(await ((await fetch(package_jsonc_path, defaultFetchConfig)).text()))) as SCHEMA,
+			new_instance = new this(package_object, package_jsonc_path_str)
+		cachedRuntimePackage.set(package_jsonc_path_str, new_instance as RuntimePackage<SCHEMA>)
+		return new_instance
 	}
 }
